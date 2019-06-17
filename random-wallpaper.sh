70e89afa2b957463144456f02d0acd750996f181
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-17 23:53:38 +0200

    Prevent the same image from being selected twice in a row
    Add a function 'randomize'

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -6,8 +6,18 @@ GREEN='\033[0;32m'
 COLORRESET='\033[0m'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/wallpapers"
-IMG=$(ls $DIR | grep -E '(jpg|jpeg|png)$' | sort -R | tail -1) 
+
+randomize() {
+    IMG=$(ls $DIR | grep -E '(jpg|jpeg|png)$' | sort -R | tail -1) 
+
+    NEW_WALLPAPER="'file://${DIR}/${IMG}'"
+    CURRENT_WALLPAPER="$( gsettings get org.gnome.desktop.background picture-uri )"
+
+    if [ $NEW_WALLPAPER == $CURRENT_WALLPAPER ]; then randomize; fi
+}
+
+randomize
 
 gsettings set org.gnome.desktop.background picture-uri file://$DIR/$IMG
 
-printf "${CYAN}Your wallpaper has been updated: ${GREEN}$IMG ${COLORRESET}\n" | sed -e 's/\.[^\.]\+$//'
+printf "${CYAN}🔀 Your wallpaper has been updated: ${GREEN}$IMG ${COLORRESET}\n" | sed -e 's/\.[^\.]\+$//'
