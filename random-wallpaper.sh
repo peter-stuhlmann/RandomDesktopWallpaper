c1e426ba93c89d63ac914a051dd9c21ac799421d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-06-17 19:21:10 +0200

    Add printf that shows the name of the chosen wallpaper

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -4,3 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/wallpapers"
 IMG=$(ls $DIR | grep -E '(jpg|jpeg|png)$' | sort -R | tail -1) 
 
 gsettings set org.gnome.desktop.background picture-uri file://$DIR/$IMG
+
+printf "Your wallpaper has been updated: $IMG\n" | sed -e 's/\.[^\.]\+$//'
